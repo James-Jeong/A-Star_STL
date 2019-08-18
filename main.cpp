646eed044fc0922bab856a2d16a364f3b0f03b9f
James-Jeong <ehddnr177@naver.com>
2019-08-18 23:03:39 +0900

    Modify codes

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ const char f = 'F';
 #define G_tt 2
 
 deque<char> map_list;
+int at_S = 0;
 int at_F = 0;
 int cnt = 0;
 
@@ -41,10 +42,13 @@ int main()
     char c;
     while((c = fgetc(fp)) != EOF){
         map_list.push_back(c);
+        if(c == 'S'){
+            at_S = cnt;
+        }
         if(c == 'F'){
             at_F = cnt;
         }
-        printf("%c", c);
+        //printf("%c", c);
         cnt += 1;
     }
     fclose(fp);
@@ -52,7 +56,7 @@ int main()
     // ***********************************
 
     // ******* 2. Routing *******
-    //deque<int> open_list;
+    deque<int> open_list;
     deque<int> close_list;
     map<int, int> temp_map;
 
@@ -76,20 +80,32 @@ int main()
     bool is_to = false; bool is_tt = false;
 
     deque<char>::iterator iter;
+    deque<char>::iterator iter2;
 
     //printf("%c", *iter_map);
     printf("\ncount : %d\n\n", cnt);
-    //for(int i = 0; i < cnt; i++){
-    //    Square* sq = new Squre(map.at(i));
-    //}
-    int i = 0;
-    // start point : 71
-    while(i < 100){
-        zz = i; zo = i+1; zt = i+2;
-        oz = i+70; ot = i+72;
-        tz = i+140; to = i+141; tt = i+142;
+    printf("at_F : %d\n", at_F);
+    printf("at_S : %d\n", at_S);
+    int i = at_S;
+    // start point : at_S -> 72
+    for(int x = 0; x < 30; x++){
+        printf("******************\n");
+        printf("i : %d\n", i);
+        zz = i-72; zo = i-71; zt = i-70;
+        oz = i-1; ot = i+1;
+        tz = i+70; to = i+71; tt = i+72;
         Square* sq = new Square(zz, zo, zt, oz, ot, tz, to, tt);
+        printf("\nzz : %d | zo : %d | zt : %d\n", zz, zo, zt);
+        printf("oz : %d | SP : %d | ot : %d\n", oz, i, ot);
+        printf("tz : %d | to : %d | tt : %d\n\n", tz, to, tt);
+        // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         int n = 8;
+        F_zz = 0; F_zo = 0; F_zt = 0; F_oz = 0; F_ot = 0;
+        F_tz = 0; F_to = 0; F_tt = 0;
+        H_zz = 0; H_zo = 0; H_zt = 0; H_oz = 0; H_ot = 0;
+        H_tz = 0; H_to = 0; H_tt = 0;
+        is_zz = false; is_zo = false; is_zt = false; is_oz = false;
+        is_ot = false; is_tz = false; is_to = false; is_tt = false;
         for(unsigned int z = 0; z < close_list.size(); z++){
             if(n == 0) break;
             if(close_list.at(z) == zz){
@@ -135,87 +151,122 @@ int main()
         }
         printf("n : %d\n", n);
 
-        if(is_zz == true){
-            H_zz = 1000;
+        // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        if(zz < 0){
+            printf("[ zz < %d, %d > reaches the wall! ]\n", i, zz);
         }
         else{
-            H_zz = cal_distance(sq->zero_zero);
+            if(is_zz == false){
+                H_zz = cal_distance(sq->zero_zero);
+                F_zz = H_zz + G_zz;
+                temp_map.insert(make_pair(zz, F_zz));
+            }
         }
-        if(is_zo == true){
-            H_zo = 1000;
-        }
-        else{
-            H_zo = cal_distance(sq->zero_one);
-        }
-        if(is_zt == true){
-            H_zt = 1000;
+        if(zo < 0){
+            printf("[ zo < %d, %d > reaches the wall! ]\n", i, zo);
         }
         else{
-            H_zt = cal_distance(sq->zero_two);
+            if(is_zo == false){
+                H_zo = cal_distance(sq->zero_one);
+                F_zo = H_zo + G_zo;
+                temp_map.insert(make_pair(zo, F_zo));
+            }
         }
-        if(is_oz == true){
-            H_oz = 1000;
-        }
-        else{
-            H_oz = cal_distance(sq->one_zero);
-        }
-        if(is_ot == true){
-            H_ot = 1000;
+        if(zt < 0){
+            printf("[ zt < %d, %d > reaches the wall! ]\n", i, zt);
         }
         else{
-            H_ot = cal_distance(sq->one_two);
+            if(is_zt == false){
+                H_zt = cal_distance(sq->zero_two);
+                F_zt = H_zt + G_zt;
+                temp_map.insert(make_pair(zt, F_zt));
+            }
         }
-        if(is_tz == true){
-            H_tz = 1000;
-        }
-        else{
-            H_tz = cal_distance(sq->two_zero);
-        }
-        if(is_to == true){
-            H_to = 1000;
+        if(oz < 0){
+            printf("[ oz < %d, %d > reaches the wall! ]\n", i, oz);
         }
         else{
-            H_to = cal_distance(sq->two_one);
+            if(is_oz == false){
+                H_oz = cal_distance(sq->one_zero);
+                F_oz = H_oz + G_oz;
+                temp_map.insert(make_pair(oz, F_oz));
+            }
         }
-        if(is_tt == true){
-            H_tt = 1000;
+        if(ot < 0){
+            printf("[ ot < %d, %d > reaches the wall! ]\n", i, ot);
         }
         else{
-            H_tt = cal_distance(sq->two_two);
+            if(is_ot == false){
+                H_ot = cal_distance(sq->one_two);
+                F_ot = H_ot + G_ot;
+                temp_map.insert(make_pair(ot, F_ot));
+            }
+        }
+        if(tz < 0){
+            printf("[ tz < %d, %d > reaches the wall! ]\n", i, tz);
+        }
+        else{
+            if(is_tz == false){
+                H_tz = cal_distance(sq->two_zero);
+                F_tz = H_tz + G_tz;
+                temp_map.insert(make_pair(tz, F_tz));
+            }
+        }
+        if(to < 0){
+            printf("[ to < %d, %d > reaches the wall! ]\n", i, to);
+        }
+        else{
+            if(is_to == false){
+                H_to = cal_distance(sq->two_one);
+                F_to = H_to + G_to;
+                temp_map.insert(make_pair(to, F_to));
+            }
+        }
+        if(tt < 0){
+            printf("[ to < %d, %d > reaches the wall! ]\n", i, to);
+        }
+        else{
+            if(is_tt == false){
+                H_tt = cal_distance(sq->two_two);
+                F_tt = H_tt + G_tt;
+                temp_map.insert(make_pair(tt, F_tt));
+            }
         }
 
-        F_zz = H_zz + G_zz; F_zo = H_zo + G_zo; F_zt = H_zt + G_zt;
-        F_oz = H_oz + G_oz; F_ot = H_ot + G_ot; F_tz = H_tz + G_tz;
-        F_to = H_to + G_to; F_tt = H_tt + G_tt;
-
-        temp_map.insert(make_pair(zz, F_zz));
-        temp_map.insert(make_pair(zo, F_zo));
-        temp_map.insert(make_pair(zt, F_zt));
-        temp_map.insert(make_pair(oz, F_oz));
-        temp_map.insert(make_pair(ot, F_ot));
-        temp_map.insert(make_pair(tz, F_tz));
-        temp_map.insert(make_pair(to, F_to));
-        temp_map.insert(make_pair(tt, F_tt));
         vector<pair<int, int>> vec(temp_map.begin(), temp_map.end());
         sort(vec.begin(), vec.end(), compare_pair_second<less>());
 
-        //open_list.push_back(vec.begin()->first); // new start point
-        i = vec.begin()->first - 71;
-        printf("open_list : %d\n\n", vec.begin()->first);
+        close_list.push_back(i); // start point
+
+        i = vec.begin()->first;
+        open_list.push_back(i); // new start point
+        printf("open_list : %d\n\n", open_list.back());
         vec.erase(vec.begin());
-        vector<pair<int, int>>::iterator iter_vec = vec.begin();
-        close_list.push_back(i+70); // start point
-        for(int j = 0; j < 7; j++){
-            printf("close_list : %d\n", iter_vec->first);
-            close_list.push_back(iter_vec->first); // other trash point
-            iter_vec += 1;
-        }
-        //temp_list.clear();
-        printf("i : %d\n", i);
+
         iter = map_list.begin();
-        iter += vec.begin()->first;
-        map_list.erase(iter);
-        map_list.insert(iter, '*');
+        iter += i+1;
+        printf("map : %c\n", map_list.at(i+1)); // check 'o'
+        vector<pair<int, int>>::iterator iter_vec = vec.begin();
+
+        for(int j = 0; j < n-1; j++){
+            if(iter_vec->first >= 0 && iter_vec->first < cnt){
+                printf("close_list : %d\n", iter_vec->first);
+                if(map_list.at(i+1) != 'o'){
+
+                }
+                close_list.push_back(iter_vec->first); // store other point
+                iter_vec += 1;
+            }
+        }
+
+        if(map_list.at(i+1) == 'o'){
+            iter2 = map_list.erase(iter-1);
+            map_list.insert(iter-1, '*');
+        }
+        else{
+            iter2 = map_list.erase(iter);
+            map_list.insert(iter, '*');
+        }
 
         delete sq;
         temp_map.clear();
@@ -223,8 +274,10 @@ int main()
         if(map_list.at(i) == f){
             break;
         }
+        printf("******************\n");
     }
 
+    printf("\n\n");
     for(int i = 0; i < cnt; i++){
         printf("%c", map_list.at(i));
     }
